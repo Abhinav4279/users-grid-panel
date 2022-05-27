742c9625fa5be0c07c00d484fd9e313abf77d1f5
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2022-05-27 11:01:39 +0530

    Layout grid

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,7 +1,21 @@
+import { Card, CardContent, Typography } from '@mui/material'
 import React from 'react'
 
-export const User = ( { user }) => {
+export const User = ( {user: { gender, nat, email, name: {title, first, last} }} ) => {
+    console.log(gender)
   return (
-    <div>{user.name.first}</div>
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {gender} ⸱ {nat}
+        </Typography>
+        <Typography variant="h5" component="div" sx={{mb: 1}}>
+          {title + ' ' + first + ' ' + last}
+        </Typography>
+        <Typography variant="body2">
+          {email}
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
--- src/components/UserPanel.jsx
+++ src/components/UserPanel.jsx
@@ -1,27 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import { CurrentUser } from './CurrentUser'
-import { User } from './User'
+import React, { useEffect, useState } from "react";
+import { CurrentUser } from "./CurrentUser";
+import { User } from "./User";
+import Data from "../data";
+import { Grid, Item } from "@mui/material";
+import { Box } from "@mui/system";
 
-const API_URL = 'https://randomuser.me/api/?inc=gender,name,nat,location,picture,email&results= 20';
+const API_URL =
+  "https://randomuser.me/api/?inc=gender,name,nat,location,picture,email&results= 20";
 
 export const UserPanel = () => {
-    const [users, setUsers] = useState([]);
-    const [current, setCurrent] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [current, setCurrent] = useState(null);
 
-    useEffect(() => {
-        (async function() {
-            const res = await fetch(API_URL);
-            const data = await res.json();
+  useEffect(() => {
+    (async function () {
+      // const res = await fetch(API_URL);
+      // const data = await res.json();
+      const data = Data;
 
-            data.results.map((obj, id) => obj.id = id)
-            setUsers(data.results);
-        })();
-    }, [])
+      data.results.map((obj, id) => (obj.id = id));
+      setUsers(data.results);
+    })();
+  }, []);
 
   return (
-    <div>
-        {current && <CurrentUser user={users[current - 1]}/>}
-        {users.map((user) => <User key={user.id} user={user} setCurrent={setCurrent}/>)}
-    </div>
-  )
-}
+    <Box sx={{p: 5}}>
+      {current && <CurrentUser user={users[current - 1]} />}
+      <Grid container spacing={2}>
+        {users.map((user) => (
+          <Grid item sm={6} md={4}>
+            <User key={user.id} user={user} setCurrent={setCurrent} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
