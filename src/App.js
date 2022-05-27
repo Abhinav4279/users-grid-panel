1a496d3ff6981f9e732f5011ec3fd5ff343afdf3
Abhinav Sharma <sharmaabhinav4279@gmail.com>
2022-05-27 16:26:28 +0530

    Create light theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import Navbar from "./components/Navbar";
 import { UserPanel } from "./components/UserPanel";
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+
+let lightTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#A259FF',
+      light: '#fff'
+    },
+    secondary: {
+      main: '#E16259',
+      light: '#fff'
+    },
+    text: {
+      select: 'rgb(255, 255, 255)'
+    },
+  }
+});
+
+let darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={lightTheme}>
       <Navbar />
       <UserPanel />
-    </>
+    </ThemeProvider>
   );
 }
 
