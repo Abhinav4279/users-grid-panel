f82d3af1dba4a1fb625a6e30f94285f95cf12492
Abhinav Sharma <54229503+Abhinav4279@users.noreply.github.com>
2022-06-17 18:18:02 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
               textDecoration: 'none',
             }}
           >
-            YourChallenge
+            RandomUsers
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
